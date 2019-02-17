bdf8351f02950a3c1a502e3e087c32a781754e4a
Paavo Väänänen <paavovaananen@Paavos-MacBook-Pro.local>
2019-02-17 22:24:08 +0200

    Add addData UI and axios instance

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import { Row, Col, Table, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
+import AddData from './components/AddData';
 import data from './data/example.json';
 
 class App extends Component {
+  state = {
+    showAddDataTable: false,
+  };
+
+  handleAddDataVisibility = value => {
+    this.setState({ showAddDataTable: value });
+  }
+
   render() {
     const mockData = data.concat(data).concat(data).concat(data);
-
+    const { showAddDataTable } = this.state;
     return (
       <div className="App">
         <Navbar bg="dark" expand="lg" variant="dark">
@@ -15,8 +24,16 @@ class App extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#data">Data</Nav.Link>
+              <Nav.Link
+                href="#home"
+                onClick={() => this.handleAddDataVisibility(false)}>
+                  Home
+                </Nav.Link>
+              <Nav.Link
+                href="#data"
+                onClick={() => this.handleAddDataVisibility(true)}>
+                Data
+              </Nav.Link>
               <NavDropdown title="Options" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#track/3.1">Track a vessel</NavDropdown.Item>
                 <NavDropdown.Item href="#fleet/3.2">Fleet info</NavDropdown.Item>
@@ -31,53 +48,58 @@ class App extends Component {
             </Form>
           </Navbar.Collapse>
         </Navbar>
-        <Row>
-          <Col>
-            <Row className="justify-content-md-center">
-              <Col md={10} mdoffset={1}>
-                <Table striped bordered hover variant="dark" className="dataTable">
-                  <thead>
-                    <tr>
-                      <th>MMSI</th>
-                      <th>IMO</th>
-                      <th>SHIP_ID</th>
-                      <th>LAT</th>
-                      <th>LON</th>
-                      <th>SPEED</th>
-                      <th>HEADING</th>
-                      <th>COURSE</th>
-                      <th>STATUS</th>
-                      <th>TIMESTAMP</th>
-                      <th>DSRC</th>
-                      <th>UTC_SECONDS</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {mockData.map((i, index) => (
-                      <tr key={index}>
-                        <td>{i.MMSI}</td>
-                        <td>{i.IMO}</td>
-                        <td>{i.SHIP_ID}</td>
-                        <td>{i.LAT}</td>
-                        <td>{i.LON}</td>
-                        <td>{i.SPEED}</td>
-                        <td>{i.HEADING}</td>
-                        <td>{i.COURSE}</td>
-                        <td>{i.STATUS}</td>
-                        <td>{i.TIMESTAMP}</td>
-                        <td>{i.DSRC}</td>
-                        <td>{i.UTC_SECONDS}</td>
+          <Row>
+            <Col>
+              <Row className="justify-content-md-center">
+                <Col md={10} mdoffset={1}>
+                {!showAddDataTable && (
+                  <Table striped bordered hover variant="dark" className="dataTable">
+                    <thead>
+                      <tr>
+                        <th>MMSI</th>
+                        <th>IMO</th>
+                        <th>SHIP_ID</th>
+                        <th>LAT</th>
+                        <th>LON</th>
+                        <th>SPEED</th>
+                        <th>HEADING</th>
+                        <th>COURSE</th>
+                        <th>STATUS</th>
+                        <th>TIMESTAMP</th>
+                        <th>DSRC</th>
+                        <th>UTC_SECONDS</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+                    </thead>
+                    <tbody>
+                      {mockData.map((i, index) => (
+                        <tr key={index}>
+                          <td>{i.MMSI}</td>
+                          <td>{i.IMO}</td>
+                          <td>{i.SHIP_ID}</td>
+                          <td>{i.LAT}</td>
+                          <td>{i.LON}</td>
+                          <td>{i.SPEED}</td>
+                          <td>{i.HEADING}</td>
+                          <td>{i.COURSE}</td>
+                          <td>{i.STATUS}</td>
+                          <td>{i.TIMESTAMP}</td>
+                          <td>{i.DSRC}</td>
+                          <td>{i.UTC_SECONDS}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                  )}
+                  {showAddDataTable && (
+                    <AddData />
+                  )}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
       </div>
     );
   }
-}
+};
 
 export default App;
---
+++ src/components/AddData.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Row, Col, Table, Button } from 'react-bootstrap';
+
+import '../App.css';
+
+const date = new Date();
+class AddData extends Component {
+  state = {
+    form: {
+      MMSI: undefined,
+      IMO: undefined,
+      SHIP_ID: undefined,
+      LAT: undefined,
+      LON: undefined,
+      SPEED: undefined,
+      HEADING: undefined,
+      COURSE: undefined,
+      STATUS: undefined,
+      TIMESTAMP: date.toISOString(),
+      DSRC: undefined,
+      UTC_SECONDS: date.valueOf().toString(),
+    },
+  };
+
+  handleValueChanged = (event, inputIdentifier) => {
+    const updatedForm = { ...this.state.form };
+    updatedForm[inputIdentifier] = event.target.value;
+    this.setState({ form: updatedForm });
+  }
+
+  handleSave = () => {
+    const { form } = this.state;
+    console.log(form);
+  }
+
+  render () {
+    const { form } = this.state;
+    return (
+      <Row>
+        <Col>
+          <Table striped bordered hover variant="dark" className="dataTable">
+            <thead>
+              <tr>
+                <th>MMSI</th>
+                <th>IMO</th>
+                <th>SHIP_ID</th>
+                <th>LAT</th>
+                <th>LON</th>
+                <th>SPEED</th>
+                <th>HEADING</th>
+                <th>COURSE</th>
+                <th>STATUS</th>
+                <th>TIMESTAMP</th>
+                <th>DSRC</th>
+                <th>UTC_SECONDS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+              {Object.keys(form).map((key, index) => (
+                <td key={index}>
+                  <input
+                    className="dataField"
+                    type="text"
+                    onChange={e => this.handleValueChanged(e, key)}
+                    value={form[key]}
+                  />
+                </td>
+              ))}
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+        <Col md={12} className="buttonRow">
+          <Button onClick={this.handleSave} variant="outline-success">SAVE</Button>
+        </Col>
+      </Row>
+    );
+  }
+};
+
+export default AddData;
